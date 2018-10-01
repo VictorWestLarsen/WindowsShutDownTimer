6bdb1869e3623b34f2bf0cc4a17f0d2bd30f07c4
Victor Wolden West-Larsen <vict536f@edu.eal.dk>
2018-10-01 14:05:45 +0200

    Hopefully it works

--- WindowsShutdownTimer/Program.cs
+++ WindowsShutdownTimer/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WindowsShutdownTimer
+namespace Shutdown
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Timer SleepTimer = new Timer();
+            int Duration = SleepTimer.HandleTime();
+            SleepTimer.SetTimer(Duration);
+            Console.WriteLine("Computeren Slukker om: " + SleepTimer.Hours + " time(r) og " + SleepTimer.Minuts + "Minut(er)");
+            Console.ReadKey();
         }
     }
 }
---
+++ WindowsShutdownTimer/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Shutdown
+{
+    class Timer
+    {
+        public int Hours = 0;
+        public int Minuts = 0;
+        public Timer()
+        {
+
+        }
+        public int HandleTime()
+        {
+            string time;
+            Console.WriteLine("Indtast tid til lukning som følgende eksempel timer:minutter");
+            time = Console.ReadLine();
+            string[] timer = time.Split(':');
+            Hours = int.Parse(timer[0]);
+            Minuts = int.Parse(timer[1]);
+            int Seconds = Hours * 3600 + Minuts * 60;
+
+            return Seconds;
+
+        }
+
+        public void SetTimer(int Duration)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "shutdown -s -t" + Duration;
+            process.Start();
+        }
+
+    }
+}
